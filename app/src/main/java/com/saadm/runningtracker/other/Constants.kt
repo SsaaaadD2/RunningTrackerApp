dbd99a21e29c6de321151ec579a80c26e756b3dc
unknown <saad.merchant@mail.mcgill.ca>
2021-02-24 15:24:02 -0500

    Setup tracking service basics

--- app/src/main/java/com/saadm/runningtracker/other/Constants.kt
+++ app/src/main/java/com/saadm/runningtracker/other/Constants.kt
@@ -3,4 +3,8 @@ package com.saadm.runningtracker.other
 object Constants {
     const val RUNNING_DATABASE_NAME = "running_db"
     const val LOCATION_PERMISSION_REQUEST_CODE = 42
+
+    const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
+    const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
+    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
 }
--- app/src/main/java/com/saadm/runningtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/saadm/runningtracker/ui/fragments/TrackingFragment.kt
@@ -1,11 +1,14 @@
 package com.saadm.runningtracker.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
 import com.saadm.runningtracker.R
+import com.saadm.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.saadm.runningtracker.services.TrackingService
 import com.saadm.runningtracker.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
@@ -19,9 +22,22 @@ class TrackingFragment: Fragment(R.layout.fragment_tracking) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync{
             map = it
+        }
+    }
+
+    private fun sendCommandToService(action: String){
+        Intent(requireContext(),TrackingService::class.java).also {
+            it.action = action
+            //Does not actually start the service each time you call this function
+            //It just delivers this intent to the service
+            requireContext().startService(it)
         }
     }
 
