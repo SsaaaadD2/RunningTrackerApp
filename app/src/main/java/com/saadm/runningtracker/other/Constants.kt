a93e057899b65e621043b4c40ed8e142fa10566f
unknown <saad.merchant@mail.mcgill.ca>
2021-02-25 14:19:40 -0500

    Get location coordinates in background

--- app/src/main/java/com/saadm/runningtracker/other/Constants.kt
+++ app/src/main/java/com/saadm/runningtracker/other/Constants.kt
@@ -9,6 +9,9 @@ object Constants {
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
     const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
 
+    const val LOCATION_UPDATE_INTERVAL = 5000L  //in milliseconds
+    const val FASTEST_LOCATION_INTERVAL = 2000L     //minimum interval
+
     const val NOTIFICATION_CHANNEL_NAME = "Tracking App"
     const val NOTIFICATION_CHANNEL_ID= "running_tracker"
     const val NOTIFICATION_ID= 30
--- app/src/main/java/com/saadm/runningtracker/services/TrackingService.kt
+++ app/src/main/java/com/saadm/runningtracker/services/TrackingService.kt
@@ -1,5 +1,6 @@
 package com.saadm.runningtracker.services
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -8,25 +9,65 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import com.saadm.runningtracker.R
 import com.saadm.runningtracker.other.Constants.ACTION_PAUSE_SERVICE
 import com.saadm.runningtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.saadm.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.saadm.runningtracker.other.Constants.ACTION_STOP_SERVICE
+import com.saadm.runningtracker.other.Constants.FASTEST_LOCATION_INTERVAL
+import com.saadm.runningtracker.other.Constants.LOCATION_UPDATE_INTERVAL
 import com.saadm.runningtracker.other.Constants.NOTIFICATION_CHANNEL_ID
 import com.saadm.runningtracker.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.saadm.runningtracker.other.Constants.NOTIFICATION_ID
+import com.saadm.runningtracker.other.TrackingUtility
 import com.saadm.runningtracker.ui.MainActivity
+import pub.devrel.easypermissions.EasyPermissions
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService: LifecycleService() {
 
     var isFirstRun: Boolean = true
 
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object{
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    //Provide initial, empty values for LiveData
+    private fun postInitialValues(){
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        //We can declare "this" as a lifecycle owner because we defined this service class as LifecycleService()
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
 
     //Called when an Intent is sent to this service
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -52,7 +93,70 @@ class TrackingService: LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    //If value is not null, it will do the apply thing
+    //If it is null, it will do the stuff after the question mark
+    private fun addEmptyPolyline() = pathPoints.value?.apply{
+        //it adds this empty list to the end of the pathPoints value
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    //The question mark here is what to do if the value was null
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+
+    //We can suppress this warning, the warning is that we need to do a permission check
+    //We do a permission check but we use EasyPermissions in our TrackingUtility,
+    //so Android doesn't recognise it
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking : Boolean){
+        if(isTracking){
+            if(TrackingUtility.hasLocationPermissions(this)){
+                val request = LocationRequest().apply{
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                        request,
+                        locationCallback,
+                        Looper.getMainLooper()
+                )
+            }
+        } else{
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+
+    val locationCallback = object: LocationCallback(){
+        override fun onLocationResult(result: LocationResult?) {
+            super.onLocationResult(result)
+            //isTracking will not be null, so no need for null check
+            if(isTracking.value!!){
+                result?.locations?.let{ locations ->
+                    for(location in locations){
+                        addPathPoint(location)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?){
+        //We use let and apply here because location is nullable, so it only does the things inside the let
+        //if it is not null
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply{
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+
     private fun startForegroundService(){
+        addEmptyPolyline()
+        isTracking.postValue(true)
         val notifManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
