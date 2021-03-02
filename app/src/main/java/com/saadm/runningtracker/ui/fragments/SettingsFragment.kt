e39030d4f15b68b47553921d481fe7b76032a9ad
unknown <saad.merchant@mail.mcgill.ca>
2021-03-02 13:23:17 -0500

    Implement settings and statistics fragments

--- app/src/main/java/com/saadm/runningtracker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/saadm/runningtracker/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,62 @@
 package com.saadm.runningtracker.ui.fragments
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.saadm.runningtracker.R
+import com.saadm.runningtracker.other.Constants.KEY_NAME
+import com.saadm.runningtracker.other.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPrefs: SharedPreferences
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadFieldsFromSharedPrefs()
+
+        btnApplyChanges.setOnClickListener {
+            val success = applyChangesToSharedPref()
+            if(success){
+                Snackbar.make(view, "Saved changes", Snackbar.LENGTH_LONG).show()
+            } else{
+                Snackbar.make(view, "Please fill out all fields", Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun loadFieldsFromSharedPrefs(){
+        val name = sharedPrefs.getString(KEY_NAME, "") ?: ""
+        val weight = sharedPrefs.getFloat(KEY_WEIGHT, 80f)
+        etName.setText(name)
+        etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPref(): Boolean{
+        val nameText = etName.text.toString()
+        val weightText = etWeight.text.toString()
+        if(nameText.isEmpty() || weightText.isEmpty()){
+            return false
+        }
+
+        sharedPrefs.edit()
+                .putString(KEY_NAME, nameText)
+                .putFloat(KEY_WEIGHT, weightText.toFloat())
+                .apply()
+
+        val toolbarText = "Let's go ${nameText}!"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
+
+    }
 }
--- app/src/main/java/com/saadm/runningtracker/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/saadm/runningtracker/ui/viewmodels/StatisticsViewModel.kt
@@ -7,4 +7,11 @@ import com.saadm.runningtracker.repositories.MainRepository
 class StatisticsViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
+
+    val totalTimeRun = mainRepository.getTotalDuration()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurnt = mainRepository.getTotalCaloriesBurnt()
+    val totalAvgSpeed = mainRepository.getTotalAverageSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
 }
